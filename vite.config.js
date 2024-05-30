f4b31927ab7b5fea73c845e0e43505a664fbc9d2
Mateus Farias <mateus.f2001@hotmail.com>
2024-05-30 12:42:58 -0300

    Corrige configuração do Vite para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/O-Farias/Day-Counter/",
-  o,
+  base: "/Day-Counter/",
 });
